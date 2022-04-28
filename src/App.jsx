a5260d1f5136abf0c1534e595c6ab8e0e3b54494
Alice Hsing <alicehsing@gmail.com>
2022-04-28 14:01:30 -0700

    successfully rendered API data on DOM

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,12 @@ import FuturamaList from './views/Futurama/List';
 import './App.css';
 
 export default function App() {
-  return (  
-      <Switch>
-        <Route path="/">
-          <h1>Futurama Quotes Compendium</h1>
-            <FuturamaList />
-        </Route>
-      </Switch>
-  )
+  return (
+    <Switch>
+      <Route path="/">
+        <h1>Futurama Quotes Compendium</h1>
+        <FuturamaList />
+      </Route>
+    </Switch>
+  );
 }
--- src/views/Futurama/List.jsx
+++ src/views/Futurama/List.jsx
@@ -1,7 +1,48 @@
-import React from 'react'
+import React from 'react';
+import { useState, useEffect } from 'react';
 
-export default function List() {
+export default function FuturamaList() {
+  // set state
+  const [quotes, setQuotes] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  //useEffect to fetch futurama API, create an object with the info I want to access to
+  useEffect(() => {
+    const getQuotes = async () => {
+      const res = await fetch('https://futuramaapi.herokuapp.com/api/quotes');
+      const data = await res.json();
+      console.log('***QUOTES***', data);
+      const futuramaData = data.map((item) => ({
+        name: item.character,
+        image: item.image,
+        quote: item.quote,
+      }));
+      setQuotes(futuramaData);
+      setLoading(false);
+    };
+    getQuotes();
+  }, []);
+
   return (
-    <div>List</div>
-  )
+    <>
+      <h2>Futurma Characters & Quotes</h2>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <>
+          {quotes.map((quote) => {
+            return (
+              <div>
+                <h3>
+                  {quote.name}: "{quote.quote}"
+                </h3>
+
+                <img alt="character" src={quote.image} />
+              </div>
+            );
+          })}
+        </>
+      )}
+    </>
+  );
 }
