c5e51a1d11e8a510d1cffc8c72e5e7819e00a230
Alice Hsing <alicehsing@gmail.com>
2022-04-28 11:17:57 -0700

    set up path for Futurama List

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,14 @@
+import { Route, Switch } from 'react-router-dom';
+import FuturamaList from './views/Futurama/List';
+import './App.css';
+
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (  
+      <Switch>
+        <Route path="/">
+          <h1>Futurama Quotes Compendium</h1>
+            <FuturamaList />
+        </Route>
+      </Switch>
+  )
 }
